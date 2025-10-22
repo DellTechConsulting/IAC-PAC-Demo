7803ffcdafed7815007920a1a7b168855113907b
mahasweta-k <93983410+mahasweta-k@users.noreply.github.com>
2025-10-22 13:33:41 +0530

    Update main.tf

--- opentofu-demo/modules/vpc/main.tf
+++ opentofu-demo/modules/vpc/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "this" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = merge(var.tags, {
-    Name = "${var.name}-vpc"
+    Name = "${var.name}-vpc1"
   })
 }
 
